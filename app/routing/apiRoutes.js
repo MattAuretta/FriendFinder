054045604a88752b0954ffd123047956ff653fbc
Matt Auretta <mauretta2@gmail.com>
2018-10-19 09:16:30 -0600

    user object pushes to api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,10 @@ module.exports = function (app) {
     app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
+
+    app.post('/api/friends', function (req, res){
+        var userData = req.body;
+        console.log(userData);
+        friends.push(userData);
+    })
 };
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static("public"));
+
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
